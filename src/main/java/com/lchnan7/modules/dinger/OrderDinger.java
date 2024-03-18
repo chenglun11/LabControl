3037a324e78e23fd626ba2cbfd64ebd2a4fefdc2
LCHNAN <lchnan7@outlook.com>
2024-03-18 13:57:31 +0800

    调整dinger信息格式

--- src/main/java/com/lchnan7/modules/dinger/OrderDinger.java
+++ src/main/java/com/lchnan7/modules/dinger/OrderDinger.java
@@ -1,5 +1,6 @@
 package com.lchnan7.modules.dinger;
 
+import com.jaemon.dingtalk.dinger.annatations.DingerMarkdown;
 import com.jaemon.dingtalk.dinger.annatations.DingerText;
 import com.jaemon.dingtalk.dinger.annatations.Parameter;
 import com.jaemon.dingtalk.entity.DingTalkResult;
@@ -7,7 +8,10 @@ import com.jaemon.dingtalk.entity.DingTalkResult;
 import java.math.BigDecimal;
 
 public interface OrderDinger {
-    @DingerText(value = "您有新的未处理审核,实验室为${appointLab}")
+
+    @DingerMarkdown(
+            value = "您有新的未处理审核,实验室为${appointLab}，请[点此链接](点此链接)登录系统进行处理",
+            title = "预约申请")
     DingTalkResult orderSuccess(String appointLab);
 
     DingTalkResult orderFailed(String orderNo, int num, boolean flag);
